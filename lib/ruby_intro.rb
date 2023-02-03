1826dc7b9c2922c6571ea45d9757a12c6696df62
Jackson Sanders <jacksonsanders35@gmail.com>
2023-02-03 13:15:24 -0600

    Parts 1 & 2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,53 +2,61 @@
 
 # Part 1
 
+#1.1
 def sum arr
-  return arr.sum
+  return arr.inject(0) { |sum, x| sum + x }
 end
 
+#1.2
 def max_2_sum arr
-  arr.sort
-  return arr[-1]+arr[-2]
+return 0 if arr.empty?
+return arr[0] if arr.length == 1
+arr.sort!
+arr[-1] + arr[-2]
 end
 
+#1.3
 def sum_to_n? arr, n
-  
-  # Find desired sum using two pointers technique
-  arr.sort
-  l = 0
-  r = length(arr)-1
-  
-  while l < r 
-    if arr[l] + arr[r] == n
-      return true
-    elsif arr[l] + arr[r] < n
-      l += 1
-    else # arr[1] + arr[r] > n
-      r -= 1
-  return false
+arr.sort!
+l = 0
+r = arr.length-1
 
+while l < r 
+  if arr[l] + arr[r] == n
+    return true
+  elsif arr[l] + arr[r] < n
+    l += 1
+  else 
+    r -= 1
+  end
+end 
+return false
 end
 
 # Part 2
 
 def hello(name)
-  puts "Hello, #{name}"
+"Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  s = s.downcase
-  if s[0] = "a" or "e" or "i" or "o" or "u" 
-    return true
-  else 
-    return false
+s = s.downcase
+if s.empty? || s[0] =~ /[^a-z]/ || s[0] =~ /[aeiou]/
+  return false
+else 
+  return true
+end
 end
 
+
 def binary_multiple_of_4? s
-  if s.to_i % 4 = 0
-    return true
-  else 
-    return false
+if s.to_i % 4 == 0 
+  return true
+else 
+  return false
 end
+end
+
 
 # Part 3
 
